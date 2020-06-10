af1d83ee456ca34abf5b7af21c36da83b56a1d2a
Rogerio Rodrigues <sp.rogerio@outlook.com>
2020-06-10 04:05:07 -0300

    Idea Model and Schema

--- server.py
+++ server.py
@@ -1,7 +1,50 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request 
+from flask_sqlalchemy import SQLAlchemy 
+from flask_marshmallow import Marshmallow 
 import os
 
+#Init app 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#Database 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Init database 
+db = SQLAlchemy(app)
+
+#Init Marshmallow 
+ma = Marshmallow(app) 
+
+#Idea Class/Model 
+class Idea(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(300))
+    tags = db.Column(db.String(75))
+    image_url = db.Column(db.String(500))
+    importance_level = db.Column(db.Integer)
+    effort_in_hours = db.Column(db.Integer)
+    tech_stack = db.Column(db.String(75))
+
+    def __init__(self, name, description, tags, image_url, importance_level, effort_in_hours, tech_stack):
+        self.name = name 
+        self.description = description 
+        self.tags = talgs 
+        self.image_url = image_url 
+        self.importance_level = importance_level
+        self.effort_in_hours = effort_in_hours
+        self.tech_stack = techstack 
+
+#Idea Schema 
+class IdeaSchema(ma.Schema):
+    class Meta: 
+        fields = ('id', 'name', 'description', 'tags', 'image_url', 'importance_level', 'effort_in_hours', 'tech_stack')
+
+idea_schema = IdeaSchema()
+ideas_schema = IdeaSchema(many=True)
+
 
 #Main Route 
 @app.route('/', methods=['GET'])
